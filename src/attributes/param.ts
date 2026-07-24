2d1af4a6bd084ae47ac5c623e2dff83073c31188
Rahimie <newrehmi@gmail.com>
2026-07-24 21:47:31 +0800

    Update param.ts

--- src/attributes/param.ts
+++ src/attributes/param.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 export const PARAM_BINDINGS = Symbol('exedra:param-bindings');
 
 export interface ParamBinding {
-  type: 'param' | 'body' | 'query' | 'header' | 'req' | 'res' | 'next';
+  type: 'param' | 'body' | 'query' | 'header' | 'req' | 'res' | 'next' | 'state' | 'flag' | 'series';
   key?: string;
 }
 
